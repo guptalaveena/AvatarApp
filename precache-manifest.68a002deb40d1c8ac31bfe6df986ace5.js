4d27f35f706f6d2180574fa70a17903b4e475151
laveena-gupta-vst-au4 <54509277+laveena-gupta-vst-au4@users.noreply.github.com>
2019-10-15 15:20:55 +0530

    Updates

---
+++ precache-manifest.68a002deb40d1c8ac31bfe6df986ace5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d9b4ccf9c9ebe94a777b07eca1f712",
+    "url": "/AvatarApp/index.html"
+  },
+  {
+    "revision": "628f74116eb1d70cc92e",
+    "url": "/AvatarApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a67f1a3a1715920727f2",
+    "url": "/AvatarApp/static/css/main.e0c29f68.chunk.css"
+  },
+  {
+    "revision": "628f74116eb1d70cc92e",
+    "url": "/AvatarApp/static/js/2.d4649083.chunk.js"
+  },
+  {
+    "revision": "a67f1a3a1715920727f2",
+    "url": "/AvatarApp/static/js/main.3707dc6e.chunk.js"
+  },
+  {
+    "revision": "89d650cf803986fa0b3a",
+    "url": "/AvatarApp/static/js/runtime-main.ebc39f2d.js"
+  }
+]);
